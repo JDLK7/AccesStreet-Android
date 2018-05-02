11b1f2bce346e2f6dfb037a551c984f38eeab56b
Jose Domenech Leal <josesguay@gmail.com>
2018-05-02 14:24:55 +0200

    Correcciones de espaciado

--- app/src/main/java/taes/accessstreet/AddAlertActivity.java
+++ app/src/main/java/taes/accessstreet/AddAlertActivity.java
@@ -17,14 +17,11 @@ public class AddAlertActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_interest_points:
+                    mTextMessage.setText("Puntos de interés");
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_obstacles:
+                    mTextMessage.setText("Obstaculos");
                     return true;
             }
             return false;
