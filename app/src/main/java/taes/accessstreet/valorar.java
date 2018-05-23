d52c8160d90fd496e90c7deae2dc467eef23dd1d
Javi <javimonlloralcaraz@gmail.com>
2018-05-23 12:28:29 +0200

    Infor a falta de reportar Adri

--- app/src/main/java/taes/accessstreet/valorar.java
+++ app/src/main/java/taes/accessstreet/valorar.java
@@ -1,5 +1,6 @@
 package taes.accessstreet;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,9 +9,11 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,59 +27,18 @@ public class valorar extends AppCompatActivity {
     TextView nombreFecha;
     TextView nombreTitulo;
 
-    String creador;
-    String fechaCreacion;
-    String titulo;
-
-    protected void getPoint() {
-        int pointId = getIntent().getIntExtra("pointID", -1);
-
-        if (pointId != -1) {
-            /**
-             * Se prepara el JSON de respuesta y se realiza una petición POST
-             * al servidor para crear el nuevo aviso en el servicio web.
-             */
-            String url = "http://"
-                    + getResources().getString(R.string.accesstreet_api_host) + ":"
-                    + getResources().getString(R.string.accesstreet_api_port) + "/api/puntos/info/" + pointId;
-
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                JSONObject typePoint = response.getJSONObject("typepoint");
-                                if (typePoint != null) {
-                                    titulo = typePoint.getString("name");
-                                    fechaCreacion = typePoint.getString("created_at");
-                                }
-
-                                JSONObject user = response.getJSONObject("user");
-                                if (user != null) {
-                                    creador = user.getString("email");
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-
-                        }
-                    });
-        }
-    }
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.valorar);
 
-        getPoint();
-        System.out.println(titulo);
+
+        String valor = getIntent().getStringExtra("resultado");
+        System.out.println(valor);
+
+        String[] split = valor.split("/");
 
         megusta = (ImageButton)findViewById(R.id.corazon);
 
@@ -86,9 +48,9 @@ public class valorar extends AppCompatActivity {
         nombreCreador = (TextView)findViewById(R.id.creador);
         nombreFecha = (TextView)findViewById(R.id.textView19);
 
-        nombreTitulo.setText(titulo);
-        nombreCreador.setText(creador);
-        nombreFecha.setText(fechaCreacion);
+        nombreTitulo.setText(split[0]);
+        nombreCreador.setText(split[2]);
+        nombreFecha.setText(split[1]);
 
         megusta.setOnClickListener(new View.OnClickListener() {
             @Override
